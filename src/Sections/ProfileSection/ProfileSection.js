a6a267df8e67535454657bc08358022a87e1c1fb
NGUYEN Le Ly Bang <bangjdev@gmail.com>
2019-09-15 03:36:57 +0200

    Fix profile page
    From now on, user can view and edit infos in the same page

--- src/Sections/ProfileSection/ProfileSection.js
+++ src/Sections/ProfileSection/ProfileSection.js
@@ -6,10 +6,31 @@ import PATH from '../../Routes/Path';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const ProfileSection = (props) =>{
+	let accountManager=useContext(AccountManageContext);
+    const [status,setStatus]=useState([false,""]);
+	const handleSubmit = async (e)=>{
+        const first_name=document.getElementById("change-first-name").value;
+        const last_name=document.getElementById("change-last-name").value;
+        const email=document.getElementById("change-email").value;
+        const username=document.getElementById("change-username").value;
+        let image=document.getElementById("change-image").files[0];
+        if (!image) image=null;
+        if (first_name===""&&last_name===""&&email==="") {console.log("error");return;}
+        let response = await accountManager.doChangeInfo(
+            {
+                first_name:(first_name==="")?accountManager.info.first_name:first_name,
+                last_name:(last_name==="")?accountManager.info.last_name:last_name,
+                email:(email==="")?accountManager.info.email:email,
+                avatar:image,
+                username:username
+            })
+        setStatus(prev=>{
+            return response    
+        });
+    }
 
     const [ifLoad,loaded]=useState(false);
     const [profile,setProfile]=useState(null);
-    let accountManager=useContext(AccountManageContext);
     useEffect(()=>{
         const getInfo = async()=>{
             let info=await accountManager.doGetUser(props.match.params.id);
@@ -36,24 +57,57 @@ const ProfileSection = (props) =>{
             );
         return(
             <div>
-                <div className="card">
-                    <div>
-                        <img src={profile.profile.avatar} alt=""  width="auto"/>
-                    </div>
-                    <p>Name: {profile.first_name} {profile.last_name}</p>
-                    <p>username: {profile.username}</p>
+                <div className="container">
+                	<form className="row">
+                		<div className="col">		            			
+            				<div className="row">
+            					<div className="col"></div>
+            					<img className="row" src={profile.profile.avatar} alt="Ảnh đại diện"/>
+            					<div className="col"></div>
+            				</div>
+            				<div className="row">
+            					<div className="col"></div>
+            					<input className="row" id="change-image" type="file" accept="image/*"/>
+            					<div className="col"></div>
+            				</div>
+            				
+                		</div>
+                		<div className="col">
+                			<div className="form-group">
+								<label for="exampleInputEmail1">Tên tài khoản</label>
+							    <input type="text" className="form-control" id="change-username" defaultValue={profile.username}/>
+							</div>
+                			<div className="form-group">
+								<label for="exampleInputEmail1">Họ</label>
+							    <input type="text" className="form-control" id="change-last-name" defaultValue={profile.last_name}/>
+							</div>
+							<div className="form-group">
+								<label for="exampleInputEmail1">Tên</label>
+							    <input type="text" className="form-control" id="change-first-name" defaultValue={profile.first_name}/>
+							</div>
+							<div className="form-group">
+								<label for="exampleInputEmail1">Tên</label>
+							    <input type="email" className="form-control" id="change-email" defaultValue={profile.email}/>
+							</div>
+							<div className="form-group">
+								<label for="exampleInputEmail1">Ngày tham gia</label>
+							    <label className="form-control">{profile.date_joined}</label>
+							</div>
+							<div className="row">
+								{(accountManager.info.username===props.match.params.id  )?(
+			                    	<div className="col">
+			                        	<button type="button" onClick={handleSubmit} className="btn btn-primary btn-block">Lưu</button>
+			                    	</div>):null
+			                    }
+								{(accountManager.info.username===props.match.params.id)?(
+									<div className="col">
+			                        	<Link to={PATH.CHANGEPASSWORD}><button className="btn btn-primary btn-block">Đổi mật khẩu</button></Link>
+			                    	</div>):null
+			                    }			                    	
+							</div>                  		                    
+                		</div>
+                	</form>
                     
-                    <p>Email: {profile.email}</p>
-                    <p>Joined at {profile.date_joined}</p>
-                    
-                    {(accountManager.info.username===props.match.params.id)?(<div>
-                        <Link to={PATH.CHANGEPASSWORD}><button className="btn btn-primary">Change Password</button></Link>
-                    </div>):null
-                    }
-                    {(accountManager.info.username===props.match.params.id  )?(<div>
-                        <Link to={PATH.CHANGEINFO}><button className="btn btn-primary">Edit Info</button></Link>
-                    </div>):null
-                    }
                 </div>
             </div>
         )
